7c869f58935a372bf820213c289dfac3d0dba1c7
kingsintersect <kingsintersect@gmail.com>
2023-11-01 18:13:41 +0100

    signup api route

--- api/server.ts
+++ api/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route';
+import authRouter from './routes/auth.route';
 
 dotenv.config()
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -22,4 +25,5 @@ app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
